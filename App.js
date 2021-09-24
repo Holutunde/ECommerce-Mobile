ed8a7b982050f7f797e81cbd37025088bac20e87
Emmanuel Aina <ainae06@gmail.com>
2021-09-24 18:30:26 +0100

    login and register stack

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Login from "./screens/LoginScreen";
+import Register from "./screens/RegisterScreen";
+import Theme from "./constants/constants";
+// import MyStack from './screens/Stack';
+import AllProducts from "./screens/ProductsScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const MyTheme = {
+		...DefaultTheme,
+		colors: {
+			...DefaultTheme.colors,
+			background: "#FFFF",
+		},
+	};
+	return (
+		<NavigationContainer theme={MyTheme}>
+			<Header />
+			<StatusBar style="auto" />
+			<Stack.Navigator screenOptions={{ header: () => null }}>
+				<Stack.Screen name="Products" component={AllProducts} />
+				<Stack.Screen name="Login" component={Login} />
+				<Stack.Screen name="Register" component={Register} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "blue",
+	},
 });
