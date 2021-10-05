d36cccf8ed8a9e0f67a80579afca277c463d629c
Emmanuel Aina <ainae06@gmail.com>
2021-10-05 13:04:57 +0100

    toast for error, navigation

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Google from "expo-google-app-auth";
 import { useNavigation } from "@react-navigation/native";
-
+import { ToastAndroid } from "react-native";
 
 export async function saveToken(loginData) {
 	try {
@@ -12,7 +12,7 @@ export async function saveToken(loginData) {
 	}
 }
 
-export async function handleGoogleSignIn( navigation ) {
+export async function handleGoogleSignIn(navigation) {
 	const config = {
 		androidClientId: `341224720546-si07qsfk5m4jji02n3hq1jqkbcfcpds9.apps.googleusercontent.com`,
 		// androidClientId: `341224720546-eo0srsqripcjus508s371ajk7peim1u5.apps.googleusercontent.com`,
@@ -24,14 +24,25 @@ export async function handleGoogleSignIn( navigation ) {
 	// 		alert(error)
 	// 	})
 	const data = await Google.logInAsync(config);
+	if (data.type == "cancel") {
+		ToastAndroid.showWithGravity(
+			"Operation Canceled",
+			ToastAndroid.SHORT,
+			ToastAndroid.TOP
+		);
+		return;
+	}
+	console.log("Google Auth Response", data);
 
-	console.log(data.idToken);
 	// const parsedData = JSON.parse(data)
-	authWithGoogle(data.idToken);
+	authWithGoogle(data.idToken, navigation);
+}
+
+export function toUserDetail(navigation) {
 	navigation.navigate("UserDetail");
 }
 
-export async function authWithGoogle(idToken) {
+export async function authWithGoogle(idToken, navigation) {
 	const googleAuthUrl = `https://ecomm-store-proj.herokuapp.com/api/v1/social_auth/google/`;
 	const auth_token = idToken;
 	const details = { auth_token };
@@ -45,7 +56,7 @@ export async function authWithGoogle(idToken) {
 	});
 
 	const socialAuthResponse = await response.json();
-	console.log("Social Auth Response", socialAuthResponse);
+	// console.log("Social Auth Response", socialAuthResponse);
 	// if (loginRes.success != true) {
 	// 	ToastAndroid.showWithGravity(
 	// 		loginRes.error,
@@ -55,4 +66,5 @@ export async function authWithGoogle(idToken) {
 	// 	return;
 	// }
 	saveToken(socialAuthResponse);
+	toUserDetail(navigation);
 }
