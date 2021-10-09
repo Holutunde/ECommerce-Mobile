b7d2f7af1df2532bea08730070681849472d1b4b
Emmanuel Aina <ainae06@gmail.com>
2021-10-09 14:23:31 +0100

    text color, render style

--- components/GoogleAuth.jsx
+++ components/GoogleAuth.jsx
@@ -4,27 +4,39 @@ import {
 	View,
 	Text,
 	Image,
-	TouchableOpacity
+	TouchableOpacity,
+	TouchableHighlight,
+	Pressable,
+	TouchableNativeFeedback
 } from "react-native";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { TouchableWithoutFeedback, Touc } from "react-native-gesture-handler";
 import Soft from "./Soft";
 
+import { useFonts } from "expo-font";
 
 const GoogleAuthButton = (props) => {
+	const [fontLoaded] = useFonts({
+		Rbt_r: require("../assets/fonts/Roboto-Regular.ttf"),
+		Rbt_b: require("../assets/fonts/Roboto-Bold.ttf"),
+		DM_r: require("../assets/fonts/DMSans-Regular.ttf"),
+		DM_b: require("../assets/fonts/DMSans-Bold.ttf"),
+	});
+
 	return (
-		<View>
-			<TouchableWithoutFeedback onPress={props.googleAuth}>
-				<Soft style={styles.googleSignIn} onPress={props.googleAuth.bind(this, props.id)}>
-					<Image
-						style={{ width: 30, height: 30 }}
-						source={{
-							uri: "http://assets.stickpng.com/thumbs/5847f9cbcef1014c0b5e48c8.png",
-						}}
-					/>
-					<Text> Continue with google</Text>
-				</Soft>
-			</TouchableWithoutFeedback>
-		</View>
+		<Pressable onPress={props.googleAuth}>
+			<Soft
+				style={styles.googleSignIn}
+				onPress={props.googleAuth.bind(this, props.id)}
+			>
+				<Image
+					style={{ width: 30, height: 30 }}
+					source={{
+						uri: "http://assets.stickpng.com/thumbs/5847f9cbcef1014c0b5e48c8.png",
+					}}
+				/>
+				<Text style={styles.googleText}> Continue with google</Text>
+			</Soft>
+		</Pressable>
 	);
 };
 
@@ -35,6 +47,11 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		justifyContent: "space-evenly",
 		marginTop: 12,
+	},
+	googleText: {
+		fontSize: 12,
+		color: "#4c8bf5",
+		fontFamily: "DM_b",
 	},
 });
 
